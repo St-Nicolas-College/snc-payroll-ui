809f99359bdddd8e51b87aa42b0823ae488fb346
almar26 <gomezalmar26@gmail.com>
2026-01-23 11:43:41 +0800

    add favicon settings to the nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+      ]
     },
   },
   compatibilityDate: "2025-07-15",
